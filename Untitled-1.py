78fbef01ae9cd37601fefabe293847b6d7e00b36
Karthic1702 <waytolearnskills@gmail.com>
2025-09-17 08:43:23 +0530

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,7 +1,1 @@
-name = "Alice"  # String
-age = 30        # Integer
-height = 1.75   # Float
-is_student = True # Boolean
-email = True 
-
 print(f"Name: {name}, Age: {age}, Height: {height}, Student: {is_student}")

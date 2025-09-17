c3da27ee59e1d5c8d225c17a0aa3bd4ccf10dc6b
Karthic1702 <waytolearnskills@gmail.com>
2025-09-17 08:42:21 +0530

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,7 @@
+name = "Alice"  # String
+age = 30        # Integer
+height = 1.75   # Float
+is_student = True # Boolean
+email = True 
+
+print(f"Name: {name}, Age: {age}, Height: {height}, Student: {is_student}")
